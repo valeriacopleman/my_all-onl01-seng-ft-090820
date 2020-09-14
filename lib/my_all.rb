27af1219f5a94356a2e89e1a506cd3cfc8bf7a0d
Valeria  Copleman <valeriacopleman@gmail.com>
2020-09-14 23:23:50 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -6,5 +6,11 @@ def my_all?(collection)
 while i < collection.length 
 block_return_values<< yield(collection[i])
 i += 1 
-end 
+end
+
+if block_return_values.include?(false)
+  false
+else
+  true 
+end
 end
